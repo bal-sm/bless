b420f16eebd1fba37805f3f86f929d14f90c9d36
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-02-06 14:11:58 +0700

    Subclassed existing codes as `BlessWindow`

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from PySide6 import QtWidgets
 
 import sys
 
+
+class BlessWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Bless")
+        button = QtWidgets.QPushButton("Mulai")
+
+        self.setCentralWidget(button)
+
+
 app = QtWidgets.QApplication(sys.argv)
 
-window = QtWidgets.QMainWindow()
-window.setWindowTitle("Bless")
-
-button = QtWidgets.QPushButton()
-button.setText("Mulai")
-
-window.setCentralWidget(button)
-
+window = BlessWindow()
 window.show()
 app.exec()
