03c2287eb283bd67edb2c6fe518050339929dc57
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-02-14 16:31:37 +0700

    Created `BismillahWindow` class
    and show an empty `QTableWidget`.

--- windows.py
+++ windows.py
@@ -1,9 +1,26 @@
 from PySide6 import QtWidgets
 
+import bismillah
+
+
+class BismillahWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Table
+        self.table = QtWidgets.QTableWidget()
+        self.table.setWindowTitle("Read it")
+
 
 class BlessWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Bless")
         self.button = QtWidgets.QPushButton("Mulai", self)
+        self.button.clicked.connect(self.bismillah_window)
         self.setCentralWidget(self.button)
+
+    def bismillah_window(self):
+        self.w = BismillahWindow()
+        self.w.table.show()
+        self.hide()
