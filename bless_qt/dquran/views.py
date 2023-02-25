d0671ed88e4e8cd9da4c0970c97f859235b7c4bb
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-02-25 11:12:15 +0700

    Created `AnNasAyat1Window`
    and set `self.button` to connect to it after clicked.

--- bless_qt/dquran/views.py
+++ bless_qt/dquran/views.py
@@ -2,6 +2,8 @@ from PySide6 import QtWidgets
 
 from bismillah_on_py import bismillah
 
+from dquran.models import Quran
+
 
 class BismillahWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -13,6 +15,39 @@ class BismillahWindow(QtWidgets.QMainWindow):
 
         # Bismillaahirrahmaanirrahiim, table item
         self.bismillah_item = QtWidgets.QTableWidgetItem(bismillah.the_sentence)
+
+        # Table configuration
+        # Header and sider
+        self.header = self.table.horizontalHeader()
+        self.sider = self.table.verticalHeader()
+
+        # Hide them
+        self.header.hide()
+        self.sider.hide()
+
+        # Set row, column, and item
+        self.table.setRowCount(1)
+        self.table.setColumnCount(1)
+        self.table.setItem(0, 0, self.bismillah_item)
+        self.table.resizeColumnsToContents()
+
+        # Set maximum width and height
+        self.table.setMaximumWidth(self.header.length())
+        self.table.setMaximumHeight(self.sider.length())
+
+
+class AnNasAyat1Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Table
+        self.table = QtWidgets.QTableWidget()
+        self.table.setWindowTitle("Read it")
+
+        self.item = Quran.objects.get(surat=114, ayat=1)
+
+        # Bismillaahirrahmaanirrahiim, table item
+        self.bismillah_item = QtWidgets.QTableWidgetItem(self.item.text)
 
         # Table configuration
         # Header and sider
--- bless_qt/duser/views.py
+++ bless_qt/duser/views.py
@@ -1,6 +1,6 @@
 from PySide6 import QtWidgets
 
-from bless_qt.dquran.views import BismillahWindow
+from bless_qt.dquran.views import BismillahWindow, AnNasAyat1Window
 
 
 class BlessWindow(QtWidgets.QMainWindow):
@@ -8,10 +8,15 @@ class BlessWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.setWindowTitle("Bless")
         self.button = QtWidgets.QPushButton("Mulai", self)
-        self.button.clicked.connect(self.bismillah_window)
+        self.button.clicked.connect(self.annas_ayat1_window)
         self.setCentralWidget(self.button)
 
     def bismillah_window(self):
         self.w = BismillahWindow()
+        self.w.table.show()
+        self.hide()
+
+    def annas_ayat1_window(self):
+        self.w = AnNasAyat1Window()
         self.w.table.show()
         self.hide()
