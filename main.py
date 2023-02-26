2cecadaa6f2d87c244d587fc54156c28588a5eab
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-02-26 18:04:43 +0700

    Configured to run `main()` function in `start.py`

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bless_server.settings")
 django.setup()
 
 from bless_qt import start
+
+
+def main():
+    start.main()
+
+
+if __name__ == "__main__":
+    main()
