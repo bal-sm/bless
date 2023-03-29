882642235599369fbb6e762cb393380f5383e783
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-03-29 14:19:14 +0700

    Moved existing `bless_qt` `start.py` codes to `main.py`
    and removed `bless_qt` `start.py` file.

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os
+import sys
 
 import django
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bless_server.settings")
 
 django.setup()
 
-from bless_qt import start
-
-
-def main():
-    start.main()
-
-
-def main_qml():
-    start.main_qml()
-
-
-def quran_qml():
-    start.quran_qml()
-
+# noreorder
+from bless_qt.dquran.models import quran_model
+from bless_qt.dquran.views import quran_main_qml_path
 
 if __name__ == "__main__":
-    quran_qml()
+    qurans = quran_model()
+
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    engine.rootContext().setContextProperty("quranmodel", qurans)
+    engine.load(quran_main_qml_path())
+
+    app.exec()
