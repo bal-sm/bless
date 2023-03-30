8a69140c1d3138a97d022f748b290c1ad38ef038
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-03-30 17:18:28 +0700

    Removed existing `PySide6.QtWidgets` codes

--- bless_qt/duser/views.py
+++ bless_qt/duser/views.py
@@ -1,29 +1,4 @@
-from PySide6 import QtWidgets
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
-
-from bless_qt.dquran.views import BismillahWindow, QuranWindow
-
 import os
-
-
-class BlessWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Bless")
-        self.button = QtWidgets.QPushButton("Mulai", self)
-        self.button.clicked.connect(self.quran_window)
-        self.setCentralWidget(self.button)
-
-    def bismillah_window(self):
-        self.w = BismillahWindow()
-        self.w.table.show()
-        self.hide()
-
-    def quran_window(self):
-        self.w = QuranWindow()
-        self.w.table.show()
-        self.hide()
 
 
 def bless_main_qml_path():
