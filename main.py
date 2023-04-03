9c231687b32d097f70d2251d268c67d587e219b7
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-04-04 03:32:36 +0700

    Created `Bridge` in `main.py`
    with `@QmlElement` decorator which inherits `QObject` and contains `getAyatsForSurat` slot function.
    
    And added necessary `QML_IMPORT_NAME` and `QML_IMPORT_MAJOR_VERSION` constant above of it and import to `bless_qt` `dquran` `main.qml`.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 import django
+from PySide6.QtCore import QObject
+from PySide6.QtCore import Slot
 from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QmlElement
 from PySide6.QtQml import QQmlApplicationEngine
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bless_server.settings")
@@ -12,7 +15,20 @@ django.setup()
 # noreorder
 from bless_qt.dquran.models import surat_model
 from bless_qt.dquran.models import quran_model
+from bless_qt.dquran.models import return_ayats
 from bless_qt.dquran.views import quran_main_qml_path
+
+QML_IMPORT_NAME = "md.ayatproperties"
+QML_IMPORT_MAJOR_VERSION = 1
+
+
+@QmlElement
+class Bridge(QObject):
+    @Slot(str, result=str)
+    def getAyatsForSurat(self, s):
+        # return ayats
+        return return_ayats(s)
+
 
 if __name__ == "__main__":
     qurans = quran_model()
